97cd53f1c31b3809d505a2df244e259fd211d490
MaxIsJoe <maxisjoe130@gmail.com>
2018-11-20 18:44:58 +0200

    Cleaned up the code for the Launcher just a tiny bit.
    I felt like it was messy

--- Launcher/TWCLauncher/TWCLauncher/MainWindow.xaml.cs
+++ Launcher/TWCLauncher/TWCLauncher/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Diagnostics;
 
 namespace TWCLauncher
 {
@@ -33,10 +20,6 @@ namespace TWCLauncher
 
         private void CTOSB_Click_1(object sender, RoutedEventArgs e)
         {
-            //Because I couldn't get server IPs from the hub and generate buttons and info correctly
-            //Within the servers page i decided to just open up the hub and let people join servers
-            //From there until I or someone else figures out how to fix this
-            //EDIT : I deleted the ServersPage.xaml like an idiot in blind rage.
             Buttons.OpenHUB();
         }
 
