22bd8f574edb7580901cce9572da0b7d7f761d0d
MaxIsJoe <maxisjoe130@gmail.com>
2018-11-21 00:41:16 +0200

    TWCLauncher Release
    It's finally up.

--- Launcher/TWCLauncher/TWCLauncher/Buttons.cs
+++ Launcher/TWCLauncher/TWCLauncher/Buttons.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Net;
 using System.Diagnostics;
 
 namespace TWCLauncher
 {
     class Buttons
     {
-        public static string MainServerIP = "byond://192.241.252.20:3001";
+        public static string MainServerIP; //The server IP is always on github as a editable txt file so users won't have to update their launchers everytime a new main server IP comes up
         public static string HUBLink = "http://www.byond.com/games/TheWizardsChronicles/TWC";
+        public static string UpdateLink = "https://github.com/MaxIsJoe/TWC/releases";
+
+
+        private static MainWindow MW = new MainWindow();
+        private static bool upDate = MW.UpToDate; //Only did this because i thought it made things look cleaner
 
         public static void ManualConnectPage()
         {
@@ -15,7 +21,16 @@ namespace TWCLauncher
         }
         public static void MainServerConnect()
         {
-            Process.Start("byondfiles\\dreamseeker.exe", MainServerIP);
+            if (upDate != true)
+            {
+                Process.Start(UpdateLink); //Redirects the users to where the new launcher can be downloaded.
+            }
+            else
+            {
+                MainServerIP = new WebClient().DownloadString("https://raw.githubusercontent.com/MaxIsJoe/TWC/MIJ-HostVer/Launcher/TWCLauncher/mainserver.txt");
+                MainServerIP.Replace("\n", "");
+                Process.Start("byondfiles\\dreamseeker.exe", MainServerIP);
+            }
         }
         public static void OpenHUB()
         {
--- Launcher/TWCLauncher/TWCLauncher/MainWindow.xaml.cs
+++ Launcher/TWCLauncher/TWCLauncher/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Media;
+using System.Net;
 
 namespace TWCLauncher
 {
@@ -9,13 +11,29 @@ namespace TWCLauncher
     public partial class MainWindow : Window
     {
 
-        public string version; //Launcher Version
+        public string version = "0.2\n"; //Launcher Version
         public bool UpToDate; //Is the Launcher up to date?
+        private string fileContent;
 
         public MainWindow()
         {
             InitializeComponent();
             SizeToContent = SizeToContent.WidthAndHeight;
+            fileContent = new WebClient().DownloadString("https://raw.githubusercontent.com/MaxIsJoe/TWC/MIJ-HostVer/Launcher/TWCLauncher/version.txt"); //Checks if there's a new version or not
+            VersionLabel.Content = ("Launcher Version : " + version);
+            if(fileContent != version) //If there is or isn't a new update do the following
+            {
+                UpToDate = false;
+                PlayButton.Content = "Update";
+                VersionLabel.Foreground = Brushes.Red; //Tells users that their launcher is outdated.
+            }
+            else
+            {
+                UpToDate = true;
+                PlayButton.Content = "Play";
+            }
+            //Console.Out.WriteLine(UpToDate);
+            //Console.Out.WriteLine(fileContent);
         }
 
         private void CTOSB_Click_1(object sender, RoutedEventArgs e)
@@ -30,14 +48,12 @@ namespace TWCLauncher
 
         private void TestButton_Click(object sender, RoutedEventArgs e)
         {
-            if (!UpToDate)
-            {
-                //Update the Launcher
-            }
-            else
-            {
-                Buttons.MainServerConnect(); //Connects to the Main server
-            }
+            Buttons.MainServerConnect();
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
         }
     }
 }
